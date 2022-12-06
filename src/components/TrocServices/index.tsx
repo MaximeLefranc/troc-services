4453a35f280bf04513a1eb94659d132501254af8
MaximeLefranc <maxilefranc@gmail.com>
2022-12-06 17:11:43 +0100

    TrocServices component: integration of the footer except on the welcome page

--- src/components/TrocServices/index.tsx
+++ src/components/TrocServices/index.tsx
@@ -1,15 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Footer from '../Footer';
 import Main from '../Main';
 import Welcome from '../Welcome';
 import './styles.scss';
 
-function TrocServices() {
+interface Location {
+  pathname: string;
+}
+
+function TrocServices(): JSX.Element {
+  const { pathname } = useLocation() as Location;
+  const isWelcomePage: boolean = pathname === '/';
   return (
     <div className="TrocServices">
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/accueil" element={<Main />} />
+        <Route path="/accueil" element={<Main />} />
       </Routes>
+      {!isWelcomePage && <Footer />}
     </div>
   );
 }
