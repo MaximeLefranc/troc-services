4d1c6a75cb9894879ee82bbacecb13f46ca79958
MaximeLefranc <maxilefranc@gmail.com>
2022-12-06 10:20:40 +0100

    TrocServices component: Add route to /accueil

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+function Main() {
+  return (
+    <section className="main">
+      {/* <Header /> */}
+      <h2 className="main__title">Les dernières annonces</h2>
+    </section>
+  );
+}
+
+export default Main;
--- src/components/TrocServices/index.tsx
+++ src/components/TrocServices/index.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import Main from '../Main';
 import Welcome from '../Welcome';
 import './styles.scss';
 
@@ -7,6 +8,7 @@ function TrocServices() {
     <div className="TrocServices">
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/accueil" element={<Main />} />
       </Routes>
     </div>
   );
