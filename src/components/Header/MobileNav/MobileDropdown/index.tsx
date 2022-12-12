e23d63951829f702ea32cac97f9b762bffb2f523
Etudiant O'clock <thmslepine@gmail.com>
2022-12-12 09:05:55 +0100

    navbar fini

--- src/components/Header/MobileNav/MobileDropdown/index.tsx
+++ src/components/Header/MobileNav/MobileDropdown/index.tsx
@@ -19,26 +19,5 @@ function MobileDropdown() {
     </li>
   );
 }
-{
-  /* <div className="mobileNav__categorie--dropdown">
-<label
-  htmlFor="touch"
-  className="mobileNav__label mobileNav__links--link"
->
-  <span className="mobileNav__categorie--span">Catégorie</span>
-</label>
-<input type="checkbox" id="touch"></input>
-<ul className="mobileNav__links__categorie--links slide">
-  <Link className="mobileNav__links--link" to="#">
-    sous-categorie
-  </Link>
-  <Link className="mobileNav__links--link" to="#">
-    sous-categorie
-  </Link>
-  <Link className="mobileNav__links--link" to="#">
-    sous-categorie
-  </Link>
-</ul>
-</div> */
-}
+
 export default MobileDropdown;
