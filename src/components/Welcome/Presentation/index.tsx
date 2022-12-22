cf4d6e18dc51bbf8e3d7cbf3633c0407ed76d353
MaximeLefranc <maxilefranc@gmail.com>
2022-12-22 11:35:10 +0100

    nav responsive

--- src/components/Welcome/Presentation/index.tsx
+++ src/components/Welcome/Presentation/index.tsx
@@ -5,8 +5,8 @@ function Presentation() {
       particuliers.
       <br />
       Ce site vous permettra, un fois inscrit, de poster des annonces et
-      d'entrer en contact avec les autres membres afin de pouvoir vous entre
-      aider mutuellement.
+      d'entrer en contact avec les autres membres afin de pouvoir vous entraider
+      mutuellement.
       <br />
       Le tout sans dépenser un centime.
     </p>
